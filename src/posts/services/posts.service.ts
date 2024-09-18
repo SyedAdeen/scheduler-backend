55cb9966e2094a1dcf7a0d6e7cd7a3aa7c59376f
Aftab Baig <aftabaig@gmail.com>
2024-09-18 15:57:49 +0500

    .

--- src/posts/services/posts.service.ts
+++ src/posts/services/posts.service.ts
@@ -135,19 +135,20 @@ export class PostsService {
     } else {
       this.logger.log("Post Immediately");
       // Enqueue immediate post
-      const job = await this.postSchedulerQueue.add('schedule-post', {
-        postId: post.id,
-        integrationId,
-        createPostDto,
-      });
-      const result = await job.finished();  // Waits for the job to finish
-      if (result && result.status === PostStatus.DUPLICATE) {
-        throw new BadRequestException("Duplicate post detected. Post was not published again.")
-      } else if (result.status == PostStatus.PUBLISHED) {
-        return { message: "Post Published Successfully" };
-      } else {
-        throw new InternalServerErrorException('Failed to publish the post.');
-      } 
+      // const job = await this.postSchedulerQueue.add('schedule-post', {
+      //   postId: post.id,
+      //   integrationId,
+      //   createPostDto,
+      // });
+      // const result = await job.finished();  // Waits for the job to finish
+      // if (result && result.status === PostStatus.DUPLICATE) {
+      //   throw new BadRequestException("Duplicate post detected. Post was not published again.")
+      // } else if (result.status == PostStatus.PUBLISHED) {
+      //   return { message: "Post Published Successfully" };
+      // } else {
+      //   throw new InternalServerErrorException('Failed to publish the post.');
+      // } 
+      return { message: "Post Added" };
     }      
     } catch (error) {
         this.logger.error('Error creating post:', { message: error.message });
